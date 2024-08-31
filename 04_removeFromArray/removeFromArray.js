b05fb1a3024a3108f11059df81b7ffb3be9f478f
BreadWeasel <breadweasel@gmail.com>
2024-08-31 00:52:47 -0700

    Add solution to 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputtedArray, ...valuesToRemove) 
+{
+    resultArray = [];
+    for (let i = 0; i < inputtedArray.length; i++)
+    {
+        if (!valuesToRemove.includes(inputtedArray[i]))
+        {
+            resultArray.push(inputtedArray[i]);
+        }
+    }
+    return resultArray;
 };
 
 // Do not edit below this line
