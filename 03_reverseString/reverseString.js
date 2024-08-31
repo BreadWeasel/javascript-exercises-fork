41a9214cb9458ed49efadde387ea30af919bc2d8
BreadWeasel <breadweasel@gmail.com>
2024-08-31 00:38:11 -0700

    Add solution to 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) 
+{
+    let reversedString = "";
+    if (stringToReverse.length > 0)
+    {
+        for (let i = stringToReverse.length - 1; i >= 0; i--)
+        {
+            reversedString += stringToReverse[i];
+        }
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
