9cb5eafebfe7cb7042afd94e52d778c565637e58
BreadWeasel <breadweasel@gmail.com>
2024-08-31 00:26:22 -0700

    Add solution to 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, repeatAmount)
+{
+    repeatedString = "";
+    if (typeof(repeatAmount) == 'number' && repeatAmount > 0)
+    {
+        let timesRepeated = 0;
+        for (timesRepeated; timesRepeated < repeatAmount; timesRepeated++)
+        {
+            repeatedString += stringToRepeat;
+            //console.log(repeatedString);
+        }
+    }
+    else if (repeatAmount < 0)
+    {
+        return "ERROR";
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
