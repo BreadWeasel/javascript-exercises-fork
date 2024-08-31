180e7462f1583fa480a9ac7f8e7aa3b465264071
BreadWeasel <breadweasel@gmail.com>
2024-08-31 04:41:31 -0700

    Add solution to 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(a, b)
+{
+    let sumResult = 0;
 
+    if (typeof(a) != "number" || typeof(b) != "number")
+    {
+        return "ERROR";
+    }
+    else if (a < 0 || b < 0)
+    {
+        return "ERROR";
+    }
+    else if (Number.isInteger(a) == false || Number.isInteger(b) == false)
+    {
+        return "ERROR";
+    }
+    else
+    {
+        let value = Math.min(a, b);
+        for (value; value <= Math.max(a, b); value++)
+        {
+            sumResult += value;
+        }
+    }
+    return sumResult;
 };
 
 // Do not edit below this line
