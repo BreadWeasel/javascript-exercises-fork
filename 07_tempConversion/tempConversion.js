43f5987ee17f3d5d8a88bbf434d5441067513a24
BreadWeasel <breadweasel@gmail.com>
2024-08-31 15:58:56 -0700

    Add solution to 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) 
+{
+  //(x − 32) × ⁠5/9⁠ °C 
+  let temperatureCelcius = (temperature - 32) * (5/9);
+  const roundedResult = Math.round(temperatureCelcius * 10) / 10
+
+  return roundedResult;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) 
+{
+   //(x × ⁠9/5⁠ + 32) °F 
+   let temperatureFahrenheit = temperature * (9/5) + 32;
+   const roundedResult = Math.round(temperatureFahrenheit * 10) / 10
+   
+   return roundedResult;
 };
 
 // Do not edit below this line
