14940f2d8a9a3c76e43fecb5b2c296635be0b60e
BreadWeasel <breadweasel@gmail.com>
2024-09-04 20:59:01 -0700

    Add solution to 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,31 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) 
+{
+    let fibonacciResult = [1];
+    if (a < 0)
+    {
+        return "OOPS";
+    }
+    else if (a == 0)
+    {
+        return Number(a);
+    }
+    else
+    {
+        for (let i = 0; i < a; i++)
+            {
+                if (i == 0)
+                {
+                    fibonacciResult.push(fibonacciResult[i]);
+                }
+                else
+                {
+                    fibonacciResult.push(fibonacciResult[i] + fibonacciResult[i - 1]);
+                }
+                console.log("Loop Count: " + i + " sequence: " +  fibonacciResult)
+            }
+        
+            return fibonacciResult[a - 1];
+    }
 };
 
 // Do not edit below this line
