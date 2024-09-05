af3ba7a357164ae19209cfc7bdcc64eb943a4c91
BreadWeasel <breadweasel@gmail.com>
2024-09-04 19:59:57 -0700

    Add solution to 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(a, b) 
+{
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) 
+{
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numberArr) 
+{
+  let sumResult = numberArr.reduce((total, curr)=>
+  {return total + curr}, 0);
+  
+  return sumResult;
 };
 
-const multiply = function() {
-
+const multiply = function(a, b = 1)
+{
+  let multResult;
+  multResult = a.reduce((total, curr)=>
+    {return total * curr});
+  return multResult;
 };
 
-const power = function() {
-	
+const power = function(a, b)
+{
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a)
+{
+  let result = 0;
+	if (a == 0)
+  {
+    result = 1;
+    return result;
+  }
+  else if (a > 0)
+  {
+    result = a * factorial(a - 1);
+    console.log(result);
+    return result;
+  }
+  return result;
 };
 
 // Do not edit below this line
