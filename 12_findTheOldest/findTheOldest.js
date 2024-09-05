db46230e7a55b18917dd0fe1cdb80eabe3575d10
BreadWeasel <breadweasel@gmail.com>
2024-09-05 00:13:43 -0700

    Add solution to 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,53 @@
-const findTheOldest = function() {
+function compareAge(personA, personB)
+{
+    const date = new Date();
+    const currentYear = date.getFullYear(); 
 
+    let ageA;
+    let ageB;
+
+    if (personA.yearOfDeath == undefined)
+    {
+        ageA = currentYear - personA.yearOfBirth;
+    }
+    else
+    {
+        ageA = personA.yearOfDeath - personA.yearOfBirth;
+    }
+
+    if (personB.yearOfDeath == undefined)
+    {
+        ageB = currentYear - personB.yearOfBirth;
+    }
+    else
+    {
+        ageB = personB.yearOfDeath - personB.yearOfBirth;
+    }
+
+
+    if (ageA > ageB)
+    {
+        return 1;
+    }
+    else if (ageA == ageB)
+    {
+        return 0;
+    }
+    else if (ageA < ageB)
+    {
+        return -1;
+    }
+}
+
+
+const findTheOldest = function(objArr)
+{
+    let resultArr;
+
+    resultArr = objArr.sort((a, b)=>
+    compareAge(a, b));
+
+    return resultArr.pop();
 };
 
 // Do not edit below this line
