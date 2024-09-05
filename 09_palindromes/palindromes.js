05487f2f21b8affad160327b95922b0d52e44069
BreadWeasel <breadweasel@gmail.com>
2024-09-04 20:33:01 -0700

    Add solution to 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (myString)
+{
+    const allowedChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let lowerStr = myString.toLowerCase()
+    let stringArr = Array.from(lowerStr);
+    stringArr = stringArr.filter((char)=> allowedChars.includes(char));
+    
+    let revStringArr = stringArr.toReversed();
 
+    let stringA = stringArr.join("");
+    let stringB = revStringArr.join("");
+
+    if (stringA === stringB)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 };
 
 // Do not edit below this line
